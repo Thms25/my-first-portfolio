8dc61c827b61ac5fcd4067326ac04458f1d721de
thomas allen <thomasallenmartinho@gmail.com>
2023-08-01 11:14:14 +0200

    just cleaned a biat

--- js/main.js
+++ js/main.js
@@ -104,13 +104,4 @@
     });
 
 
-    // Testimonials carousel
-    // $(".testimonial-carousel").owlCarousel({
-    //     autoplay: true,
-    //     smartSpeed: 1500,
-    //     dots: true,
-    //     loop: true,
-    //     items: 1
-    // });
-
 })(jQuery);
