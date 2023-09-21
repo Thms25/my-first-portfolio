ca143f9631541bf22bc609a690396700c11349f4
thomas allen <thomasallenmartinho@gmail.com>
2023-09-21 17:31:41 +0200

    done for the day

--- components/About.jsx
+++ components/About.jsx
@@ -3,20 +3,20 @@ import Image from 'next/image';
 
 const About = () => {
   return (
-    <section className='bg-light p-12'>
+    <section className='bg-light p-20'>
       <div className='text-center m-auto grid place-items-center relative mb-12'>
         <h1 className='uppercase text-8xl text-white font-semibold'>About</h1>
         <h2 className='absolute text-dark font-bold text-2xl uppercase'>About me</h2>
       </div>
-      <div className='text-primary lg:flex'>
-        <Image
-            priority
-            width={720}
-            height={480}
-            src='/images/me.jpeg'
-            className='m-auto my-6 w-1/2'
-        />
+      <div className='text-primary md:flex'>
         <div className=''>
+          <Image
+              priority
+              width={720}
+              height={480}
+              src='/images/me.jpeg'
+              className='m-auto my-6 w-full sm:w-1/2'
+          />
           <h3 className='font-semibold text-xl'>Full Stack Developer</h3>
           <p className='text-lg my-3'>
             Hi I'm Thomas, a trained sound engineer now working towards becoming a full stack web developer. I am driven by the balance of technical ability and creativity that those careers demand. I am eager to grow within the tech industry that evolves ever so quickly.
