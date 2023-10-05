9187814231bd69eedeca9a45ef6445912a6a9eea
thomas allen <thomasallenmartinho@gmail.com>
2023-10-05 19:27:52 +0200

    typed effect done

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,22 +3,32 @@
 import Image from 'next/image';
 import styles from 'styles/header.module.scss';
 import { motion } from 'framer-motion';
+import Typed from 'react-typed';
 
 export default function Header () {
   return (
     <header className='h-screen grid place-items-center'>
-      <div className='md:flex lg:justify-evenly p-4 grid items-center gap-8 mx-4 lg:mx-6 mt-20 '>
+      <div className='md:flex md:justify-evenly p-4 grid items-center gap-8 mx-4 lg:mx-6 mt-20 '>
         <Image
           priority
           width={720}
           height={480}
           src='/images/me.jpeg'
-          className='m-auto mb-8 w-full md:w-3/4 xl:w-full'
+          className='m-auto mb-8 w-full lg:w-3/4 xl:w-full'
         />
         <div className='text-center md:text-left'>
-          <h3>Hello I am</h3>
-          <h1 className='text-5xl text-dark uppercase font-bold' id={styles.name}>Thomas Allen</h1>
-          <h3 className='text-2xl'>Full Stack Developer</h3>
+          <h3 className='text-lg md:text-2xl'>Hello I am</h3>
+          <h1 className='text-4xl lg:text-6xl xl:text-7xl text-dark uppercase font-bold' id={styles.name}>
+            Thomas Allen
+          </h1>
+          <h3 className='text-xl lg:text-3xl xl:text-4xl'>
+            <Typed
+              strings={['Full Stack Developer']}
+              typeSpeed={60}
+              backSpeed={20}
+              loop
+            />
+          </h3>
           <div className='mt-8'>
             <a href='/public/files/Thomas_Allen_CV.pdf' download className='mt-6 btn'>Download my CV</a>
           </div>
