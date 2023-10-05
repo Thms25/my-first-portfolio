68b06f54eb00ff3a0897f20f74cca5836a86409c
thomas allen <thomasallenmartinho@gmail.com>
2023-10-05 19:46:17 +0200

    header fixed

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import styles from 'styles/header.module.scss';
 import { motion } from 'framer-motion';
 import Typed from 'react-typed';
+import { Link as ScrollLink } from 'react-scroll';
 
 export default function Header () {
   return (
@@ -34,20 +35,22 @@ export default function Header () {
           </div>
         </div>
       </div>
-      <motion.div
-        className="mt-6 m-auto w-4"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className={`w-6 h-6 ${styles.arrow}`}
+      <ScrollLink to="about" smooth={true} duration={500}>
+        <motion.div
+          className="mt-6 m-auto w-4"
         >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-        </svg>
-      </motion.div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`w-6 h-6 ${styles.arrow}`}
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+          </svg>
+        </motion.div>
+      </ScrollLink>
     </header>
 
   );
