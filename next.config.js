c466c9ae7bd85e1de2c9e8ca543dfe0d2862b9f6
thomas allen <thomasallenmartinho@gmail.com>
2023-11-18 14:42:33 +0100

    try 2 for image fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  basePath: isProd ? "/my-first-portfolio/" : "",
+  basePath: "/my-first-portfolio",
+  assetPrefix: "/my-first-portfolio//",
   output: "export",
   images: {
     unoptimized: true,
