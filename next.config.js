adc729d1b261fe281c49e44ecff60a3c4042c96b
thomas allen <thomasallenmartinho@gmail.com>
2023-11-18 14:29:31 +0100

    next config edited for image fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  basePath: isProd ? "/my-first-portfolio/" : "",
   output: "export",
   images: {
     unoptimized: true,
