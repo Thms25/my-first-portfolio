6e14056602cf708b8e954c5e2a360a8a76383bb8
Thomas Allen <thomasallenmartinho@gmail.com>
2024-04-17 12:38:43 +0200

    updated to next 14

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {}\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1pHMReEdPEVzWV7xsAL0x9rd1zPJq/AgTEaetyh/3Gs=\"\n}"
