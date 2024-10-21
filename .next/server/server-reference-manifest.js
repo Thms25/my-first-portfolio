c4a68aa35a4c693f0b275bc130aa884da5f81664
Thomas Allen <thomasallenmartinho@gmail.com>
2024-10-21 10:12:30 +0200

    installed missing dep

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4WpwJaBzBzpPEJB8MY9mOzbDh5NAtiusLhwDAJo+hzc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
